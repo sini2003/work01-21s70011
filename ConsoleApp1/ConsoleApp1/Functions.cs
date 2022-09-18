6141bbd1560a3784b4e0d0f3c617c48315280469
sini2003 <kobataku.0201@gmail.com>
2022-09-18 15:31:34 +0900

    21s70011_9/18

---
+++ ConsoleApp1/ConsoleApp1/Functions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class Functions
+    {
+        public static (int, bool) ParseArgs(string[] args)
+        {
+            //桁数の特定
+            if (args.Length < 1) return (10, true);
+
+            int digits;
+            if (!int.TryParse(args[0], out digits))
+            {
+                digits = 10;
+            }
+
+            //第二引数が存在して、"0"だったら記号は使わない
+            if (args.Length < 2) return (digits, true);
+            bool useMark = args[1] != "0";
+
+            return (digits, useMark);
+        }
+
+        public static char GetRandomLetter
+            (char[] big, char[] little, char[] number, char[] mark,
+            System.Random rand, bool useMark)
+        {
+            char[] target;
+            //まずはどの配列から文字を選ぶかサイコロをふる
+            int randMax;
+            if (useMark)
+                randMax = 4;
+            else
+                randMax = 3;
+
+            switch (rand.Next(randMax))
+            {
+                case 3:
+                    target = mark;
+                    break;
+                case 0:
+                    target = big;
+                    break;
+                case 1:
+                    target = little;
+                    break;
+                case 2:
+                default:
+                    target = number;
+                    break;
+            }
+
+            //次にtargetの中からランダムな文字を選ぶ
+            return target[rand.Next(target.Length)];
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using PasswordGenerator;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //桁数指定
+            (int digits, bool useMark) = Functions.ParseArgs(args);
+            var rand = new System.Random();
+
+            char[] bigLetters = new char[] {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                'W', 'X', 'Y', 'Z'};
+            char[] littleLetters = new char[] {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                'w', 'x', 'y', 'z'};
+            char[] numberLetters = new char[] {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            char[] markLetters = new char[] {
+                '-', '_', '!', '+', '*', '#', '$', '%', '&'};
+
+            string result = "";
+            for (int i = 0; i < digits; i++)
+            {
+                char letter = Functions.GetRandomLetter
+                    (bigLetters, littleLetters, numberLetters, markLetters,
+                    rand, useMark);
+
+                result += letter;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
